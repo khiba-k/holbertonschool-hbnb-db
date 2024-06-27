4caf9df06ade107a85928bc17d8635b9491e6a2a
Kamohelo Koali <152155388+KamoheloKoali@users.noreply.github.com>
2024-06-27 15:49:15 +0000

    make amenities class suitable for sqlalchemy

--- Model/amenities.py
+++ Model/amenities.py
@@ -3,14 +3,14 @@ from create_app_db import db
 from datetime import datetime
 
 class Amenity(db.Model):
-    def __init__(self, name):
-        super().__init__()
-        self.name = name
+    name = db.Column(db.String(50))
+    created_at = db.Column(db.Date, datetime.now)
+    updated_at = None
 
     def to_dict(self):
         return {
             'id': self.id,
             'name': self.name,
-            'created_at': self.created_at.isoformat(),
-            'updated_at': self.updated_at.isoformat()
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
         }                                                
